b558bfce1199055c72e501c02203d2c1c536d81d
JBLDSKY <JBLDSKY@gmail.com>
2023-11-18 22:21:19 +0100

    when closed

--- myapp/src/components/Chat/Bubble.svelte
+++ myapp/src/components/Chat/Bubble.svelte
@@ -1,9 +1,7 @@
-<script>
-	import { Card } from 'flowbite-svelte';
-</script>
-
-<Card class="fixed right-16 bottom-12 animate-[bounce_2s_ease-in-out_infinite]">
-	<p class="font-normal text-gray-700 dark:text-gray-400">
-		Hi! 👋 ik ben JouwGPT. Hoe kan ik u vandaag van dienst zijn?
-	</p>
-</Card>
+<div class="fixed right-16 bottom-12 animate-[bounce_2s_ease-in-out_infinite] bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+    <div class="p-4">
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+            Hi! 👋 ik ben JouwGPT. Hoe kan ik u vandaag van dienst zijn?
+        </p>
+    </div>
+</div>
